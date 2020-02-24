d800a2d4991b46c622e27c609b2fa5ea2f167185
Chris Guest <chris.guest@condenastint.com>
2020-02-24 10:51:09 +0000

    This version is hardcoded until I fix the UI problems.

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-const addTwoNumbers = require('@chrisguest75/array_add_rce');
+const [addTwoNumbers, addArrayNumbers] = require('@chrisguest75/array_add_rce');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', number1: 0, number2: 0, answer: '0' });
+  let numbers = [0];
+  res.render('index', { title: 'Express', numbers: numbers, answer: '0' });
 });
 
 router.post('/exploit', function(req, res, next) {
-  const number1 = Number(req.body.number1)
-  const number2 = Number(req.body.number2)
 
-  var answer = addTwoNumbers(number1, number2);
-  console.log(number1 + " + " + number2 + " = " + answer)
+  let numbers = [];
+
+  for (const property in req.body) {
+    numbers.push(Number(req.body[property]));
+  }
+
+  numbers = [967, 78, 10, 12, 200, 37, 8888];
+  var answer = addArrayNumbers(numbers);
+  console.log(numbers + " " + answer)
     
-  res.render('index', { title: 'Express', number1: number1, number2: number2, answer: answer });
+  res.render('index', { title: 'Express', numbers: numbers, answer: answer });
 });
 
 module.exports = router;
